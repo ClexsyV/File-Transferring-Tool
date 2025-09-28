2fc57a44cd3498920d73a2ce6e35fdb4afd14a8a
Viljam Vänskä <vili123.vanska@gmail.com>
2025-09-28 17:07:55 +0300

    Update and rename pages.py to pages.py

--- src/pages.py
+++ src/pages.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint, redirect, url_for
 
 pages = Blueprint('pages', __name__)
 
-@pages.route('home')
+@pages.route('/home')
 def index():
   return render_template('index.html')
